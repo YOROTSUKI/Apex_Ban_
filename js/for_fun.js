0157ce53237e120ec7226271426149e9846941b2
YRTK <2029107002@qq.com>
2024-06-20 14:42:28 +0800

    for fun

---
+++ js/for_fun.js
@@ -0,0 +1,30 @@
+
+var myArray = [
+    'https://www.apexno1.com/', 
+    'https://www.4399.com/', 
+    'https://www.baidu.com/', 
+    'https://store.xim.tech/products/xim-matrix', 
+    'https://www.respawn.com/', 
+    'https://www.cronusmax.com/home-2/', 
+    'https://www.bilibili.com/video/BV1wT421m7t6'];
+
+if (window.self !== window.top) {
+    let start = new Date();
+    debugger;
+    let end = new Date();
+
+    //   console.log(rValue)
+    // urls = 
+    if (end - start > 100) {
+        ''
+    } else {
+        function RandArray(array) {
+            var rand = Math.random() * array.length | 0;
+            var rValue = array[rand];
+            return rValue;
+        }
+
+        var rValue = RandArray(myArray);
+    }
+
+}
